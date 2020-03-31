bb6390ac1de486af2b4fe542a854a4e36c0b3678
irausch <irausch@node0.argdta2.wall2-ilabt-iminds-be.wall1.ilabt.iminds.be>
2020-03-31 06:53:02 -0600

    Added plots.

--- auxiliary/analysis_single_topology.py
+++ auxiliary/analysis_single_topology.py
@@ -7,17 +7,15 @@ import sys
 def average_over_seeds(size, network, deg):
 	filename = '/users/irausch/analysis/results/hetero_shading/size_' + size + '/' + network + '/' + deg + '/'
 	fname = filename + str(1) + '/data.csv'
-	#~ columns = ['foraging robots', 'building robots', 'average estimate', 'average gradient', 'construction events', 'blocks in cache']
-	#~ data = pd.DataFrame(columns=columns)
-	#~ data = data.append(pd.read_csv(fname, sep='\t'), ignore_index = True)
 	data = pd.read_csv(fname, sep='\t')
-	av_est = np.array(data['average estimate'])[:45000] 
-	#~ av_est = np.array(data['blocks in cache'])[:45000]
+	column='average estimate'
+	if network != "proximity" and network != "no_comm":
+		column = 'building robots'
+	av_est = np.array(data[column])[:45000] 
 	for seed in range(2,31):
 		fname = filename + str(seed) + '/data.csv'	
 		data = pd.read_csv(fname, sep='\t')
-		av_est = av_est + np.array(data['average estimate'])[:45000] 
-		#~ av_est = av_est + np.array(data['blocks in cache'])[:45000]
+		av_est = av_est + np.array(data[column])[:45000] 
 
 	av_est = av_est / 30
 	time_array = np.arange(0,len(av_est),1)
@@ -27,12 +25,20 @@ def average_over_seeds(size, network, deg):
 
 
 network=str(sys.argv[1])
-degsId=[str(x) for x in range(0,6)]
-degs=['4','6','8','10', '12', '14']
+
+if network == "no_comm":
+	degsId=["0"]
+	degs=['0']
+elif network == "proximity":
+	degsId=[str(x) for x in range(0,6)]
+	degs=['4','6','8','10', '12', '14']
+else:
+	degsId=[str(x) for x in range(2,6)]
+	degs=['8','12','16','20']
 
 #~ sizes=['50', '50', '50', '50']
 sizes=['20', '30', '40', '50']
-#~ sizes=[]
+
 fig, ax = plt.subplots(nrows=2, ncols=2, sharex=True, sharey=True)
 
 i=0
@@ -45,7 +51,7 @@ for d in range(0,len(degs)):
 
 ax[0,0].set_ylim([0,1.0])
 ax[0,0].set_ylabel("Avg Estimate")
-ax[0,0].set_title(network)
+ax[0,0].set_title(size)
 ax[0,0].legend(loc='upper center', ncol=2)
 start, end = ax[0,0].get_xlim()
 ax[0,0].xaxis.set_ticks(np.arange(start, end, 10000))
@@ -59,7 +65,7 @@ for d in range(0,len(degs)):
 	ax[0,1].plot(seed_averaged_data[1], seed_averaged_data[0], label=degs[d])
 
 ax[0,1].set_ylim([0,1.0])
-ax[0,1].set_title(network)
+ax[0,1].set_title(size)
 ax[0,1].legend(loc='upper center', ncol=2)
 start, end = ax[0,1].get_xlim()
 ax[0,1].xaxis.set_ticks(np.arange(start, end, 10000))
@@ -76,7 +82,7 @@ for d in range(0,len(degs)):
 ax[1,0].set_ylim([0,1.0])
 ax[1,0].set_ylabel("Avg Estimate")
 ax[1,0].set_xlabel("Time")
-ax[1,0].set_title(network)
+ax[1,0].set_title(size)
 ax[1,0].legend(loc='upper center', ncol=2)
 start, end = ax[1,0].get_xlim()
 ax[1,0].xaxis.set_ticks(np.arange(start, end, 10000))
@@ -91,7 +97,7 @@ for d in range(0,len(degs)):
 
 ax[1,1].set_ylim([0,1.0])
 ax[1,1].set_xlabel("Time")
-ax[1,1].set_title(network)
+ax[1,1].set_title(size)
 ax[1,1].legend(loc='upper center', ncol=2)
 start, end = ax[1,1].get_xlim()
 ax[1,1].xaxis.set_ticks(np.arange(start, end, 10000))
